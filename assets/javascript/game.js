e18983255e4d71cf602fb5e718fe2fc4ea05144b
markeemoon <markdreed512@gmail.com>
2018-12-26 16:24:20 -0600

    refactored players and functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,113 +1,95 @@
 
-    var game = {
-        player: '',
-        opponent: '',
+   
+        var player = {
+            name: '',
+            healthPoints: 0,
+            baseAttackStrength: 0,
+            attackPoints: 0,
+            attack: function(){
+                opponent.health -= this.attackPoints;
+                this.attackPoints += this.baseAttackStrength;
+                //display health
+                displayHealth();
+                console.log('player health:'+ healthPoints)
+                console.log('player attackPoints:'+ attackPoints)  
+            },
+        };
 
-        r2d2:  {
-            name: "R2D2",
-            healthPoints: 110,
-            attackPoints: 15,
-            counterAttackPoints: 15,
-        },
-        c3p0: {
-            name: "C3PO",
-            healthPoints: 120,
-            attackPoints: 12,
-            counterAttackPoints: 12,
-        },
-        bb8: {
-            name: "BB8",
-            healthPoints: 100,
-            attackPoints: 18,
-            counterAttackPoints: 18,
-        },
-        gonk: {
-            name: "GNK Power Droid",
-            healthPoints: 130,
-            attackPoints: 10,
-            counterAttackPoints: 10,
-        },
-    }
-    //end of game object
+        var opponent = {
+            name: '',
+            healthPoints: 0,
+            CounterAttackPoints: 0,
+            counterAttack: function(){
+                player.healthPoints -= this.counterAttackPoints;
+                //display health
+                displayHealth();
+                console.log('opponent health:'+ healthPoints)
+            },
+        }
+        var selectPlayer = function(name, baseAttack, health){
+            player.name = name;
+            player.attackPoints = baseAttack;
+            player.baseAttackStrength = baseAttack;
+            player.healthPoints = health;
+            displayHealth();
+
+
+        };
+        var selectOpponent = function(name, counter, health){
+            player.name = name;
+            opponent.counterAttackPoints = counter;
+            opponent.healthPoints = health;
+            displayHealth();
+        };
+
+        function displayHealth(){
+            $('#player-health').html("Your Health: " + player.healthPoints);
+            $('#opponent-health').html("Opponent Health: " + opponent.healthPoints)
+        }
+    
+
 
     
     $( document ).ready(function() {
-        var firstClick = true;
-        var playerAttackPower = 0;
-        var playerHealth = 0;
-        var opponentCounterPower = 0;
-        var opponentHealth = 0;
+        var clickCount = 0;
         $('.choose-row-pic').on('click', function(event){
             //when first character clicked:
-            if (firstClick === true){ 
+            if  (clickCount === 0){ 
                 console.log('first click')
-                firstClick = false;
+             clickCount++;
                 $('#'+ event.target.id).css("visibility", "hidden");
                 $('#'+ event.target.id + "2").css("visibility", "visible");
-                if (event.target.id === "r2d2pic"){                   
-                    //set attack power and health
-                    playerAttackPower = game.r2d2.attackPoints;
-                    playerHealth = game.r2d2.healthPoints;
-                    //display health
-                    $('#player-health').html("Your Health: " + playerHealth)
-                    console.log('attack: '+ playerAttackPower, "health: " + playerHealth)       
+                if (event.target.id === "r2d2pic"){  
+                    selectPlayer("R2D2", 15, 110);                       
                 }
                 else if (event.target.id === "c3popic"){
-                    console.log('do player = c3po stuff')
-                    playerAttackPower = game.c3p0.attackPoints;
-                    playerHealth = game.c3p0.healthPoints;
-                    $('#player-health').html("Your Health: " + playerHealth)
-                    console.log('attack: '+ playerAttackPower, "health: " + playerHealth)  
+                    selectPlayer("C3PO", 12, 120);  
                 }
                 else if (event.target.id === "bb8pic"){
-                    console.log('do player = bb8 stuff')
-                    playerAttackPower = game.bb8.attackPoints;
-                    playerHealth = game.bb8.healthPoints;
-                    $('#player-health').html("Your Health: " + playerHealth)
-                    console.log('attack: '+ playerAttackPower, "health: " + playerHealth)  
+                    selectPlayer("BB-8", 18, 100); 
                 }
                 else{
-                    console.log('do player = gonk stuff')
-                    playerAttackPower = game.gonk.attackPoints;
-                    playerHealth = game.gonk.healthPoints;
-                    $('#player-health').html("Your Health: " + playerHealth)
-                    console.log('attack: '+ playerAttackPower, "health: " + playerHealth)  
+                    selectPlayer("GNK Power Droid", 10, 130);  
                 }
            
             }
              //else, (if it's the second click)
-            else {  
+            else if  (clickCount === 1){  
+                clickCount++
                 console.log('second click')
                 $('#'+ event.target.id).css("visibility", "hidden");
                 $('#'+ event.target.id + "2").css("visibility", "visible");
                 if (event.target.id === "r2d2pic"){                   
-                    //set counter attack power and health
-                    opponentCounterPower = game.r2d2.counterAttackPoints;
-                    opponentHealth = game.r2d2.healthPoints;
-                    //display health
-                    $('#opponent-health').html("Opponent Health: " + opponentHealth)
-                    console.log('opponent counter attack: '+ opponentCounterPower, "health: " + playerHealth)       
+                    selectOpponent("R2D2", 15, 110)//name, counter, health     
                 }
                 else if (event.target.id === "c3popic"){
-                    console.log('do player = c3po stuff')
-                    playerAttackPower = game.c3p0.counterAttackPoints;
-                    playerHealth = game.c3p0.healthPoints;
-                    $('#opponent-health').html("Opponent Health: " + opponentHealth)
-                    console.log('opponent counter attack: '+ opponentCounterPower, "health: " + playerHealth) 
+                    selectOpponent("C3PO", 12, 120);
                 }
-                else if (event.target.id === "bb8pic"){c
-                    console.log('do player = bb8 stuff')
-                    playerAttackPower = game.bb8.counterAttackPoints;
-                    playerHealth = game.bb8.healthPoints;
-                    $('#opponent-health').html("Opponent Health: " + opponentHealth)
-                    console.log('opponent counter attack: '+ opponentCounterPower, "health: " + playerHealth) 
+                else if (event.target.id === "bb8pic"){
+                    selectOpponent("BB-8", 18, 100);
                 }
                 else{
-                    console.log('do player = gonk stuff')
-                    playerAttackPower = game.gonk.counterAttackPoints;
-                    playerHealth = game.gonk.healthPoints;
-                    $('#opponent-health').html("Opponent Health: " + opponentHealth)
-                    console.log('opponent counter attack: '+ opponentCounterPower, "health: " + playerHealth) 
+                    selectOpponent("GNK Power Droid", 10, 130);
                 }
             }
         })
